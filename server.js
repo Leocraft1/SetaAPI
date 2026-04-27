660b7b21c71ef1442b2dbe074407b3406a06edf2
Leocraft1 <leonardotoschi07@gmail.com>
2026-04-27 15:50:30 +0200

    Cleaned script

--- server.js
+++ server.js
@@ -677,20 +677,11 @@ app.get('/routemap/:code', async (req, res) => {
     const finalHtml = fixed.replace(
         `<script src="https://www.setaweb.it/js/leafletCustomConfig.js"></script>`,
         `<script>
-        //Stile grafico
-// https://leaflet-extras.github.io/leaflet-providers/preview/
-//Necessita licenza
-// var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-//     minZoom: 12,
-//     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-// });
-
-
-var Stadia_OSMBright = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    minZoom: 11,
-    attribution: '© OpenStreetMap',
-    referrerPolicy : 'strict-origin-when-cross-origin'
-});
+            var Stadia_OSMBright = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                minZoom: 11,
+                attribution: '© OpenStreetMap',
+                referrerPolicy : 'strict-origin-when-cross-origin'
+            });
         </script>
         `
     );
