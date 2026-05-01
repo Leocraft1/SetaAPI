084b83ec618a1e3dd1e4e4fdc010e7d1d5bb81b0
Leocraft1 <leonardotoschi07@gmail.com>
2026-05-01 17:21:24 +0000

    Automatic Update Commit from Serverissimo

---
+++ start.sh
@@ -0,0 +1,22 @@
+cd /root/seta
+
+# Rest of script from old node
+# Pull and sync from SetaAPI repository
+cd /DATA/AppData/Seta-APIs
+git stage *
+echo Staged
+git restore server.js
+git restore package.json
+echo Restored
+git commit -m "Automatic Update Commit from Serverissimo"
+echo Committed
+git push https://github.com/Leocraft1/SetaAPI main
+echo Pushed
+
+git pull
+echo Pulled
+npm install
+
+
+npm install
+node server.js
