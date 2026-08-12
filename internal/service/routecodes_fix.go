2b88ca27404216333d9667945d0f803e9e22bfe6
Leocraft1 <leonardotoschi07@gmail.com>
2026-08-12 18:08:15 +0200

    Dani hai rotto tutto :)

--- internal/service/routecodes_fix.go
+++ internal/service/routecodes_fix.go
@@ -35,7 +35,7 @@ func GetRoutecodes() model.RLResponse {
 			Exists:    val.Still_exists,
 		}
 
-		if val.Still_exists {
+		if val.Still_exists == false {
 			g.old = append(g.old, entry)
 		} else {
 			g.active = append(g.active, entry)
