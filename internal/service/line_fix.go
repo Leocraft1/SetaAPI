a5145af1f3303413845f952bc07dec06a8678f4b
Leocraft1 <leonardotoschi07@gmail.com>
2026-08-15 11:32:23 +0200

    Fixed some routes  in /arrivals not being checked correctly which resulted in empty correction

--- internal/service/line_fix.go
+++ internal/service/line_fix.go
@@ -3,21 +3,23 @@ package service
 import (
 	"setaapi/internal/model"
 	"setaapi/internal/repository"
+	"strings"
 )
 
 func fixLineInfo(val *model.LineInfo) {
 	//Separates route code from journey code
 	rc := val.Journey_code
-	rc = rc[:len(rc)-8]
+	rc_split := strings.Split(rc, "-")
+	rc = rc_split[0]+"-"+rc_split[1]+"-"+rc_split[2]
 
 	//Gets specific route from DB
 	linea_disp := repository.GetRoute(rc)
 
 	//Fixes linea only if parameters are not null in DB
-	if linea_disp.Disp_linea != nil {
+	if linea_disp.Disp_linea != nil && *linea_disp.Disp_linea != "" {
 		val.Line = *linea_disp.Disp_linea
 	}
-	if linea_disp.Disp_dest != nil {
+	if linea_disp.Disp_dest != nil && *linea_disp.Disp_dest != "" {
 		val.Destination = *linea_disp.Disp_dest
 	}
 
